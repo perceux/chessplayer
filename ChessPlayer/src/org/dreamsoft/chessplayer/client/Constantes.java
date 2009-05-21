16c004fcadefa67780e09c6398bd6a12d56bb9f4
perceux <perceux@334f34f0-3b42-11de-b438-597f59cd7555>
2009-05-21 21:21:04 +0000

    ChessPiece => int Add ChessBoardRenderer

---
+++ ChessPlayer/src/org/dreamsoft/chessplayer/client/Constantes.java
@@ -0,0 +1,22 @@
+package org.dreamsoft.chessplayer.client;
+
+public interface Constantes {
+	public static final int PAWN = 1;
+	
+	public static final int ROOK = 2;
+	
+	public static final int KNIGHT = 3;
+	
+	public static final int BISHOP = 4;
+	
+	public static final int KING = 5;
+	
+	public static final int QUEEN = 6;
+	
+	public static final int EMPTY = 0;
+	
+	public static final int WHITE = 1;
+	
+	public static final int BLACK = 2;
+}
+
